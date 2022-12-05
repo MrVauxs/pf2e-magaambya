bce2b72a48a8df979b01d6937fc5974fd3dc2215
mrvauxs <mrvauxs@gmail.com>
2022-12-05 23:27:32 +0000

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# pf2e-magaambya
+A module dedicated to improve your experience when playing Strength of Thousands adventure path in the Pathfinder 2e system.
---
+++ scripts/pf2e-magaambya.js
@@ -0,0 +1,12 @@
+Hooks.on("init", async () => {
+	game.settings.register("pf2e-magaambya", "hideNPCs", {
+		scope: "world",
+		config: true,
+		name: "Hide non-Player Token Targets",
+		hint: "Hides not owned by players tokens from the target list of a damage roll.",
+		type: Boolean,
+		default: false
+	});
+});
+
+
