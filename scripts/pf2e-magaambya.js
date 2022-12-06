126b52e84e13e54fbd7d22b26f549b267543d17a
mrvauxs <mrvauxs@gmail.com>
2022-12-06 01:19:38 +0000

    h

--- scripts/pf2e-magaambya.js
+++ scripts/pf2e-magaambya.js
@@ -1,12 +1,29 @@
-Hooks.on("init", async () => {
-	game.settings.register("pf2e-magaambya", "hideNPCs", {
-		scope: "world",
-		config: true,
-		name: "Hide non-Player Token Targets",
-		hint: "Hides not owned by players tokens from the target list of a damage roll.",
-		type: Boolean,
-		default: false
-	});
-});
+const pf2eMagaambya = {}
 
+pf2eMagaambya.featsList = {
+	"additional lore": "KFNTTRUJCvzqWfK1",
+	"uzuntaji": "KwPNji9kZImC0506",
+	"tempest-sun mages": "XStw5buvEMUKxGtm",
+	"student of the magaambya": "K653qXggMJdFmBnp",
+	"steeped in history": "uq0y8S1XQbQ0H9Il",
+	"skill increase": "rJiegHur2CpxsIyV",
+	"skill feat": "HNpZPhdpcK4LrHFd",
+	"secondary branch": "dnjAgmsXuCcpRPwE",
+	"rain-scribes": "S7gHQujdKLoXVnJA",
+	"primary branch": "Fvq2spwov1Z0Gxr8",
+	"make your own luck": "QJrsjP5dE2PlqWBn",
+	"magical paragon: uzunjati": "6CaS7eIJgsxQC6DT",
+	"magical paragon: tempest-sun mages": "mOD3dsUdHRUN7UkV",
+	"magical paragon: rain-scribes": "FCtAGVqlb7s9q7SX",
+	"magical paragon: emerald boughs": "fsDe1O3plVn41ssp",
+	"magical paragon: cascade bearers": "25VeAgr1X8WAcuRq",
+	"magical paragon": "LQIsLyi2KcFRghae",
+	"general feat": "gtiGiVhNCSEBg9DW",
+	"emerald boughs": "Ymtyrd1y9laTdyTl",
+	"dedicated attendant": "OrBnVL2Ghn8NJX7l",
+	"cascade bearers": "vGpGXPqJEuY4LU4a",
+	"branch influence": "qbZrkEhH5u2iuBMi",
+	"branch feat": "0uwh9mCCygjJfMsD"
+}
 
+// Nothing to do here yet
